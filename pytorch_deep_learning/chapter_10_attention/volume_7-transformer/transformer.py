90327d7b2564bf8b95f2e8db2878c5251a4c0fae
chuck47286 <chuck472861399@163.com>
2023-12-04 08:26:06 +0800

    [feat] transformer ready to done

---
+++ pytorch_deep_learning/chapter_10_attention/volume_7-transformer/transformer.py
@@ -0,0 +1,32 @@
+import math
+import pandas as pd
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+# 基于位置的前馈神经网络
+#@save
+class PositionWiseFFN(nn.Module):
+    """基于位置的前馈网络"""
+    def __init__(self, ffn_num_input, ffn_num_hiddens, ffn_num_outputs,
+                 **kwargs):
+        super(PositionWiseFFN, self).__init__(**kwargs)
+        self.dense1 = nn.Linear(ffn_num_input, ffn_num_hiddens)
+        self.relu = nn.ReLU()
+        self.dense2 = nn.Linear(ffn_num_hiddens, ffn_num_outputs)
+
+    def forward(self, X):
+        return self.dense2(self.relu(self.dense1(X)))
+
+
+# test
+# ffn = PositionWiseFFN(4, 4, 8)
+# ffn.eval()
+# print(ffn(torch.ones((2, 3, 4))).shape)
+
+# 残差连接和层规范化
+ln = nn.LayerNorm(2)
+bn = nn.BatchNorm1d(2)
+X = torch.tensor([[1, 2], [2, 3]], dtype=torch.float32)
+# 在训练模式下计算X的均值和方差
+print('layer norm:', ln(X), '\nbatch norm:', bn(X))
