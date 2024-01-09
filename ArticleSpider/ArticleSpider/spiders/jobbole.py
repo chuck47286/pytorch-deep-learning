99dc4ca00e3699b05401ceb113df4ef0500d7e9c
chuck47286 <chuck472861399@163.com>
2024-01-09 14:29:51 +0800

    [feat] 4-7 xpath,css选择器 done

--- ArticleSpider/ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/ArticleSpider/spiders/jobbole.py
@@ -7,4 +7,16 @@ class JobboleSpider(scrapy.Spider):
     start_urls = ["https://news.cnblogs.com"]
 
     def parse(self, response):
+        # '//*[@id="entry_761661"]/div[2]/h2/a' # Xpath相对路径
+        # "/html/body/div[2]/div[2]/div[4]/div[1]/div[2]/h2/a" # Xpath全路径
+        # Xpath的实现方式
+        # 1. 通过元素id直接获取（不推荐，由于无法直接知道id的属性）
+        # url = response.xpath('//*[@id="entry_761661"]/div[2]/h2/a/@href').extract_first("")
+        # 2. 通过不变的属性的相对路径获取
+        # url = response.xpath('//div[@id="news_list"]/div[1]/div[2]/h2/a/@href').extract_first("")
+        # url = response.xpath('//div[@id="news_list"]//h2[@class="news_entry"]/a/@href').extract_first("")
+
+        # url = response.xpath('//div[@id="news_list"]//h2[@class="news_entry"]/a/@href').extract()
+        # 3. css选择器写法
+        url = response.css('div#news_list h2 a::attr(href)').extract()
         pass
---
+++ ArticleSpider/main.py
@@ -0,0 +1,17 @@
+# 调式scrapy 爬虫框架脚本
+from scrapy.cmdline import execute
+
+import sys
+import os
+"""
+D:/python_workspace/ArticleSpider/main.py
+D:/python_workspace/ArticleSpider
+D:\python_workspace\ArticleSpider
+"""
+# print(__file__)
+# print(os.path.dirname(__file__))
+# print(os.path.dirname(os.path.abspath(__file__)))
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+execute(["scrapy", "crawl", "jobbole"])
