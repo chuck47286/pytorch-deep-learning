fe69e40a823dded772d5627f8511fccc6f1252df
chuck47286 <chuck472861399@163.com>
2024-01-11 15:58:04 +0800

    [feat] 4-13回调解析详情页数据

--- ArticleSpider/ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/ArticleSpider/spiders/jobbole.py
@@ -1,7 +1,10 @@
 from urllib import parse
+import re
+import json
 
 import scrapy
 from scrapy import Request
+import requests
 import undetected_chromedriver
 
 
@@ -68,7 +71,7 @@ class JobboleSpider(scrapy.Spider):
         :param response:
         :return:
         """
-        post_nodes = response.css('#news_list .news_block')
+        post_nodes = response.css('#news_list .news_block')[:1]
         for post_node in post_nodes:
             image_url = post_node.css('.entry_summary a img::attr(href)').extract_first('')
             post_url = post_node.css('h2 a::attr(href)').extract_first('')
@@ -76,8 +79,33 @@ class JobboleSpider(scrapy.Spider):
 
         # 提取下一页并交给scrapy进行下载
         # next_url = response.css('dev.pager a::last-child::text').extract_first('')
-        next_url = response.xpath('//a[contains(text(), "Next >")]/@href').extract_first('')
-        yield Request(url=parse.urljoin(response.url, next_url), callback=self.parse)
+        # next_url = response.xpath('//a[contains(text(), "Next >")]/@href').extract_first('')
+        # yield Request(url=parse.urljoin(response.url, next_url), callback=self.parse)
 
     def parse_detail(self, response):
-        pass
+        match_re = re.match(".*?(\d+)", response.url)
+        if match_re:
+            title = response.css("#news_title a::text").extract_first("")
+            create_date = response.css("#news_info .time::text").extract_first("")
+            create_date = response.css("#news_content").extract()[0]
+            tag_list = response.css(".news_tags a::text").extract()
+            tags = ",".join(tag_list)
+
+            # Ajax中获取数据
+            post_id = match_re.group(1)
+            # html = requests.get(url=parse.urljoin(response.url, "/NewsAjax/GetAjaxNewsInfo?contentId={}".format(post_id)))
+            # j_data = json.loads(html.text)
+
+            yield requests.get(url=parse.urljoin(response.url, "/NewsAjax/GetAjaxNewsInfo?contentId={}".format(post_id)), callback=self.parse_num)
+
+
+            # praise_num = j_data["DiggCount"]
+            # fav_nums = j_data["TotalView"]
+            # comment_nums = j_data["CommentCount"]
+
+    def parse_num(self, response):
+        j_data = json.loads(response.text)
+
+        praise_num = j_data["DiggCount"]
+        fav_nums = j_data["TotalView"]
+        comment_nums = j_data["CommentCount"]
